9e9cf8cb5c77c6cc6d124247b03a8245e6992b72
Limoo <limoodrice@gmail.com>
2023-03-17 17:05:55 +0300

    custom tests

--- macros/test_more_goals.sql
+++ macros/test_more_goals.sql
@@ -1,18 +1,16 @@
 {% macro test_more_goals(model, column_name) %}
 
-with validation as(
+with validation as (
     select
         {{ column_name }} as more_goals
-        assist
     from {{ model }}
 ),
 
-validation_errors as(
+validation_errors as (
     select
         more_goals
     from validation
     where more_goals > 2
-        and assist = 3
 
 )
 
